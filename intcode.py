ed6c5895416b839f531856069b81722a2179abe8
Michael Fogleman <fogleman@gmail.com>
2019-12-13 15:27:54 -0500

    very basic disassembler

--- intcode.py
+++ intcode.py
@@ -33,3 +33,34 @@ def run(program, program_input=None):
             mem[writes[2]] = int(reads[0] == reads[1])
         if op == 9:
             rb += reads[0]
+
+def disassemble(program):
+    sizes = dict(enumerate([1, 4, 4, 2, 2, 3, 3, 4, 4, 2]))
+    names = {
+        1: 'ADD', 2: 'MUL', 3: 'IN', 4: 'OUT',
+        5: 'JNZ', 6: 'JZ', 7: 'LT', 8: 'EQ',
+        9: 'RB', 99: 'HALT',
+    }
+    if isinstance(program, str):
+        program = list(map(int, program.split(',')))
+    i = 0
+    while i < len(program):
+        op = program[i] % 100
+        size = sizes.get(op, 1)
+        name = names.get(op, '?')
+        args = [program[i + j] for j in range(1, size)]
+        modes = [(program[i] // 10 ** j) % 10 for j in range(2, 5)]
+        tokens = [name]
+        for arg, mode in zip(args, modes):
+            if mode == 0:
+                tokens.append('%d' % arg)
+            elif mode == 1:
+                tokens.append('#%d' % arg)
+            elif mode == 2:
+                tokens.append('[%d]' % arg)
+        print('%04d' % i, '%6d' % program[i], ' '.join(tokens))
+        i += size
+
+if __name__ == '__main__':
+    import fileinput
+    disassemble(list(fileinput.input())[0])
